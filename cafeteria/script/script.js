9e417d12d6f2528000bd27b38c1419db32012ba2
lxcaszXD <lxcasz74@gmail.com>
2025-04-05 22:10:51 -0300

    animação js feita (carousel), section itemEspecial + avaliacao

--- cafeteria/script/script.js
+++ cafeteria/script/script.js
@@ -32,3 +32,21 @@ document.addEventListener("DOMContentLoaded", function() {
         menu.classList.remove("menuAtivo");
     });
 });
+
+$(document).ready(function(){
+    $(' #itemsEspecial .carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
+
+$(document).ready(function(){
+    $(' #avaliacao .carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
