f763a9baeb7fe7f2ebafc85faf82062aa67184ab
Miguel Ferreira <miguelccezarferreira@gmail.com>
2025-05-16 11:31:44 -0300

    criação api tv

--- app/views/template/head.php
+++ app/views/template/head.php
@@ -1,21 +1,23 @@
 <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Ícone da aplicação para dispositivos Apple -->
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-  <!-- Link para o arquivo CSS do painel de controle (style personalizado) -->
-  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/dash.css">
-  <!-- Ícone do site (para abas do navegador) -->
-  <link rel="icon" type="image/png" href="<?= BASE_URL ?>assets/imgDash/coffeBranco.png" />
-  <!-- Link para a biblioteca de ícones do Bootstrap -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous">
-  <!-- Link para a biblioteca Font Awesome (para ícones adicionais) -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-  <!-- Fontes e ícones -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-  <!-- Nucleo Icons para ícones gráficos (usado no tema Argon) -->
-  <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
-   <!-- BOX ICONS  -->
-   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<!-- Ícone da aplicação para dispositivos Apple -->
+<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
+<!-- Link para o arquivo CSS do painel de controle (style personalizado) -->
+<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/dash.css">
+<!-- Ícone do site (para abas do navegador) -->
+<link rel="icon" type="image/png" href="<?= BASE_URL ?>assets/imgDash/coffeBranco.png" />
+<!-- Link para a biblioteca de ícones do Bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous">
+<!-- Link para a biblioteca Font Awesome (para ícones adicionais) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+<!-- Fontes e ícones -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+<!-- Nucleo Icons para ícones gráficos (usado no tema Argon) -->
+<link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
+<!-- BOX ICONS  -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -23,12 +25,12 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 
-  
-  <!-- Link para o CSS de ícones SVG personalizados -->
-  <link href="<?= BASE_URL ?>assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Script para carregar o Font Awesome (opcional para ícones de fontes) -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <!-- Arquivo CSS principal do painel de controle (argon-dashboard) -->
-  <link id="pagestyle" href="<?= BASE_URL ?>assets/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
-  <!-- Arquivo CSS personalizado para o site -->
-  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
+
+<!-- Link para o CSS de ícones SVG personalizados -->
+<link href="<?= BASE_URL ?>assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- Script para carregar o Font Awesome (opcional para ícones de fontes) -->
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<!-- Arquivo CSS principal do painel de controle (argon-dashboard) -->
+<link id="pagestyle" href="<?= BASE_URL ?>assets/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
+<!-- Arquivo CSS personalizado para o site -->
+<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
