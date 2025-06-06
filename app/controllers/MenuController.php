2121988bb40b5e1dfffed4e522eaa9be2323ebe2
LxcaszXD <lxcasz74@gmail.com>
2025-06-06 11:47:38 -0300

    início do filtro de produtos no menu

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -9,6 +9,13 @@
 
      $dados['mensagem'] = 'Bem-vindo ao Menu';
 
+     $produtosModel = new Produtos();
+     $produtos = $produtosModel->getListarProdutos();
+     $dados['produtos'] = $produtos;
+
+     $produtosAleatorios = $produtosModel->getListarProdutosAleatorios(4);
+     $dados['produtosAleatorios'] = $produtosAleatorios;
+
      
      $this->carregarViews('menu', $dados);
     }
